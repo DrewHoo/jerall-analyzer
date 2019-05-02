1ff9719f4b436f45e2cfb90945063cfd42c48dc1
DrewHoo <drewhoover@gmail.com>
2019-05-01 23:09:59 -0500

    manually add genders in genders.json

--- src/transformer.ts
+++ src/transformer.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs'
+import { Gender } from './types'
 type MarioCard = {
     id: string
     name: string
@@ -13,20 +15,51 @@ type MarioCard = {
     keywords: string[]
     text: string
 }
-
-const data: MarioCard[] = fs.readFile()
 const fixText = (text: string) =>
     text.split(/[\n\r]/g).filter(str => str.length)
 
-data.filter(({ set }) => set === 'Houses of Morrowind')
-    .map(card => {
-        const { id, isunique, attack, health, race, text, ...rest } = card
-        return {
-            collectible: true,
-            ...rest,
-            ...(race ? { race: [race], power: attack, health } : {}),
-            ...(isunique ? { rarity: 'Unique Legendary' } : {}),
-            ...(text ? { text: fixText(text) } : {}),
+const rawData: Buffer = fs.readFileSync('mario-cards.json')
+const data: MarioCard[] = JSON.parse(rawData.toString())
+
+const creatures: { name: string; gender: string }[] = data
+    .filter(({ type }) => type === 'Creature')
+    .map(({ name, race }) => {
+        if (
+            [
+                'High Elf',
+                'Dark Elf',
+                'Wood Elf',
+                'Imperial',
+                'Daedra',
+                'Nord',
+                'Breton',
+                'Redguard',
+                'Khajiit',
+                'Argonian',
+                'Giant',
+                'Ash Creature',
+                'Dragon',
+                'Skeleton',
+                'Orc',
+                'God',
+                'Vampire',
+            ].includes(race)
+        ) {
+            return { name, gender: '' }
+        } else {
+            return { name, gender: 'Unknown' }
         }
-    })
-    .slice(50)
+    });
+
+fs.writeFileSync('./cards/genders.json', JSON.stringify(creatures));
+
+// data.filter(({ set }) => set === 'Houses of Morrowind').map(card => {
+//     const { id, isunique, attack, health, race, text, ...rest } = card
+//     return {
+//         collectible: true,
+//         ...rest,
+//         ...(race ? { race: [race], power: attack, health } : {}),
+//         ...(isunique ? { rarity: 'Unique Legendary' } : {}),
+//         ...(text ? { text: fixText(text) } : {}),
+//     }
+// })
